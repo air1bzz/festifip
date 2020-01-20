c852fbcb33de05e11933130417ba383fd778212c
Erwan Morvan <air1bzz@gmail.com>
2020-01-20 21:30:35 +0100

    archive 2019 site version

---
+++ old/2019/assets/js/prog_main.js
@@ -0,0 +1,194 @@
+var bands = {
+  improvizeurs: {
+    title: 'JAM IN PUCEUL',
+    desc: "Collectif Les Improvizeurs",
+    date: "21h00 – Café « Chez Tonton » - cabaret d’impro et jam session - entrée libre",
+    para: [
+      "Le collectif des Improvizeurs, c'est une douzaine de personnes qui ont pris l'habitude de se retrouver une fois par mois avec leurs instruments, autour de Saffré. Lors de ces sessions, on y pratique des bœufs spontanés, du sound-painting et des reprises. Tous les styles de musiques sont les bienvenus ! Le collectif vous présentera son cabaret avec des matchs d’impro et des surprises..."
+    ]
+  },
+  morin: {
+    title: "L’AGENCE DE VOYAGE MORIN",
+    desc: "Mic Mac Cie",
+    date: 'en continu - entresort fulgurant – à partir de 7 ans',
+    para: [
+      "L'agence Morin propose une forme de voyage révolutionnaire qui a su s'adapter à l'air du temps: dans un monde où tout va vite, où les nouvelles technologies sont omniprésentes, l'Agence Morin relève le défi et vous propose le tour du monde en 80 secondes."
+    ]
+  },
+  momette: {
+    title: 'LES PETITES FORMES',
+    desc: 'Momette',
+    date: 'en continu - entresorts intimistes et tout terrain – à partir de 5 ans',
+    para: [
+      "Petite forme masquée, sans parole mais avec le cœur et petite prouesse de table au swing désinvolte."
+    ]
+  },
+  livrairie: {
+    title: 'LA LIVRAIRIE',
+    desc: 'Cie du Monde Entier',
+    date: 'en continu - librairie ambulante',
+    para: [
+      "La Librairie-café du monde entier est un lieu d’accueil public itinérant, installé autour d’une remorque. Un lieu de détente, d’imagination et d’échanges autour du livre, provoquant la circulation du livre. Sur ce lieu-librairie nous stimulons les griffons impromptus: lectures chuchotées, ateliers improvisés, discussions."
+    ]
+  },
+  bitonio: {
+    title: 'LE BAR ANIME',
+    desc: 'Cie Bitonio',
+    date: 'samedi et dimanche - en continu',
+    para: [
+      "Le bar de Marcel et Alice propose une manière originale de se faire servir une boisson: glissez votre jeton dans le décolleté de Madame, ou au travers des dents du bonheur de Monsieur, et c'est le sourire assuré !"
+    ],
+    links: ['http://www.ciebitonio.fr']
+  },
+  kutcuk: {
+    title: 'KUTCUK MANEJ',
+    desc: 'manège enfant',
+    date: 'samedi et dimanche - en continu',
+    para: [
+      "Le Kütçük Manej est une attraction pour les enfants de 3 à 10 ans. Un petit manège à manivelle qui tourne dans une ambiance de bulles, de paillettes et de bonbons."
+    ],
+    links: ['https://www.kutcukmanej.com']
+  },
+  meduses: {
+    title: 'LES MEDUSES DANSENT SOUVENT AVEC LA LUNE',
+    desc: 'Cie la cabane',
+    date: '15h00 - danse musique poésie – à partir de 3 ans',
+    para: [
+      "Deux danseuses, un accordéoniste et une écrivaine pour 3 saynètes qui ouvrent des espaces de jeu avec le public. Laissez-vous prendre à l’invitation d’une poésie renversée par la danse."
+    ],
+    links: ['http://www.compagnielacabane.fr']
+  },
+  maintenir: {
+    title: 'MAINTENIR',
+    desc: 'Cie Facile d’excès',
+    date: '16h30 – duo de cordes à sauter – à partir de 3 ans',
+    para: [
+      "Un duo prêt à tout... ou presque... ou du moins à tout ce dont ils se sentent capables... à leur façon ! De la démesure toute en finesse... L’important n’est pas d’atteindre le but, le principal c’est l’ascension... L’objectif n’étant pas de devenir célèbre mais juste quelqu’un et ce sera déjà pas mal... Bref, un moment de légèreté où le rapport direct avec le public prime !"
+    ],
+    links: ["http://faciledexces.blogspot.com"]
+  },
+  raoul: {
+    title: 'RAOUL LE CHEVALIER',
+    desc: 'Cie Tetrofort',
+    date: '18h00 - spectacle en carton – à partir de 3 ans',
+    para: [
+      "Dans un univers de carton brut, matière première utilisée pour les décors, les costumes et les accessoires, une dizaine de personnages décalés vont se croiser dans une ambiance déjantée. Partons alors à l’aventure avec Raoul, un chevalier arrogant et maladroit, prêt à tout pour sauver une princesse vraiment blonde, emprisonnée dans le château de la cruelle sorcière Saperli."
+    ],
+    links: ['http://www.tetrofort.com/pages/raoul-le-chevalier.html']
+  },
+  kafi: {
+    title: 'KAFI',
+    desc: 'Fanfare afro insolite',
+    date: '19h00 et 21h30 – concert mouvant',
+    para: [
+      "Fanfare afro insolite, KAFI est un mix de percussions africaines endiablées et de mélodies cuivrées aux diverses influences, le tout servi façon arts de la rue et déambulation. « 50% de matière Brass », met en scène 4 percussionnistes imprégnés de tradition d’Afrique de l’Ouest, associés à 4 soufflants dont les influences passent par les 3 continents que sont l’Amérique, l’Europe et l’Afrique. Ils fusionnent autour d'un répertoire de compositions originales et de reprises inédites."
+    ],
+    links: ['http://www.kafi-musique.fr']
+  },
+  tripes: {
+    title: "ROCKTRIPES",
+    desc: 'Cie Energumen',
+    date: '20h30 – cirque n’roll – à partir de 5 ans',
+    para: [
+      "Rock Tripes, c'est le voyage de Youri, Mud et Argo guidé par les apparitions vocales de leur père. L'univers de cette fratrie est décalé ; les relations se jouent de façon physique dans les regards, l'acrobatie, la danse, la manipulation d'objets. La bande son rock accompagne les personnages dans une quête poétique et burlesque à travers les routes, les bars, les océans et votre imaginaire !"
+    ],
+    links: ['http://cie.energumen.free.fr/rocktripes.html']
+  },
+  lions: {
+    title: 'DES LIONS POUR DES LIONS',
+    desc: "Jazz Rock Trans",
+    date: '22h30 - concert',
+    para: [
+      "De Luis Mariano à Tom Waits, il y a un écart, un grand écart dans lequel la tribu 'Des Lions pour Des Lions' se plait à imaginer sa musique. S’il fallait nommer cette sphère, elle s’appellerait «transe», la transe apatride que l’on retrouve dans le rock, la musique africaine et indienne, le blues, la musique noise, le jazz, formant ainsi un ensemble musical totalement libre et inventif !"
+    ],
+    links: ['https://www.deslionspourdeslions.com']
+  },
+  floor: {
+    title: 'DANCE ON ZE FLOOR',
+    desc: 'FIP Sound System',
+    date: '00h00 – bal dansant',
+    para: [
+      "Tel un grand crew millésimé, le staff du FIP vous distillera une selection de titres internationaux ne manquant pas de cuisse ! De quoi enflammer le dance-floor..."
+    ]
+  },
+  mouchu: {
+    title: 'MOUCHU',
+    desc: 'Cie D’ici et d’ailleurs',
+    date: '14h00 - spectacle enfant - à partir de 5 ans',
+    para: [
+      "Mouchu nous emmène sur les pas vifs de Max, un enfant à l’âge de la découverte du monde, de toutes les curiosités. Quand Max trouve par hasard une heure que quelqu’un a « perdue » là, sur le trottoir, le conte du quotidien bascule dans l’aventure fantastique. Drôle, poétique, surréaliste, Mouchu invite enfants et adultes à réfléchir ensemble sur le monde et ses transformations."
+    ],
+    links: ['https://fr-fr.facebook.com/theatre.d.dailleurs']
+  },
+  tartellini: {
+    title: 'LES SOEURS TARTELLINI',
+    desc: 'de bouche à oreille - Madame Suzie',
+    date: 'Dimanche - entre 15h et 16h30 - Impromptus a capella',
+    para: [
+      "Si vous croisez leur chemin, arrêtez-vous ! Vous pourriez bien partir à l’autre bout du monde. Le temps d’une chanson, elles vous emmèneront dans un voyage aux sonorités de votre choix. Fermez les yeux, c’est rien que pour vous."
+    ],
+    links: [
+      'http://www.madamesuzie.com/index.php'
+    ]
+  },
+  nefertiti: {
+    title: 'NEFERTITI IN THE KITCHEN',
+    desc: 'Cabaret excentrique',
+    date: '16h30 - concert - à partir de 5 ans',
+    para: [
+      "Nefertiti in the Kitchen est un duo déjanté, une rencontre entre une chanteuse de rue au tempérament bien trempé et un homme-orchestre barbu. Jen Rival, Nicolas Méheust et leur joyeux bastringue nous emmène dans un univers forain rappelant les années folles. Entre accordéon électrique, boite à musique, Mellotron et autres curiosités, laissez-vous porter dans ce Kabaret punK psychédélique !"
+    ],
+    links: [
+      'http://www.nefertitiinthekitchen.com'
+    ]
+  }
+}
+
+var lastElem = null
+
+function bandMain (bandTag) {
+  var band = bands[bandTag]
+  var imgElem = document.getElementById('band-img')
+  var titleElem = document.getElementById('band-title')
+  var descElem = document.getElementById('band-description')
+  var dateElem = document.getElementById('band-date')
+  var mainElem = document.getElementById('band')
+  var navElem = document.getElementById(bandTag)
+  var src = '/assets/img/' + bandTag + '.jpg'
+
+  titleElem.innerText = band.title
+  descElem.innerText = band.desc
+  dateElem.innerText = band.date
+  imgElem.setAttribute('src', src)
+  imgElem.setAttribute('alt', bandTag)
+
+  // clean text children
+  for (var elem = document.getElementsByClassName('band-text')[0]; document.getElementsByClassName('band-text').length !== 0; elem = document.getElementsByClassName('band-text')[0]) {
+    elem.remove()
+  }
+
+  for (var text of band.para) {
+    var div = document.createElement('div')
+    div.setAttribute('class', 'band-text')
+    div.innerText = text
+    mainElem.appendChild(div)
+  }
+
+  if (band.links) {
+    for (var link of band.links) {
+      var a = document.createElement('a')
+      a.setAttribute('class', 'band-text')
+      a.setAttribute('href', link)
+      a.setAttribute('target', '_blank')
+      a.innerText = link
+      mainElem.appendChild(a)
+    }
+  }
+
+  // Change color
+  if (lastElem) {
+    lastElem.style.color = '#CB6829'
+  }
+  navElem.style.color = '#DFC124'
+  lastElem = navElem
+}
